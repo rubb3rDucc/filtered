c6004859251f59d9cc34bfb9d35dc98b9c594c64
rubb3rDucc <thomalin@oregonstate.edu>
2024-10-26 22:19:59 -0400

    cleaned up eslint file and add emca ver to fix err

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,22 +1,11 @@
 import globals from "globals";
-// import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslint from '@eslint/js';
-// import tseslint from 'typescript-eslint';
-
-// export default [
-//   { files: ['**/*.ts', '**/*.tsx}'] },
-//   pluginJs.configs.recommended,
-//   ...tseslint.configs.recommended,
-// ];
 
 export default [
   {files: ['**/*.ts']},
-  // {extends: [
-      { languageOptions: { globals: globals.browser } },
-  //   eslint.configs.recommended,
-  //   ...tseslint.configs.strict,
-  // ]},
+  {  parserOptions: {
+    ecmaVersion: 8,
+  }},
+  {languageOptions: { globals: globals.browser } },
   {rules: {
     '@typescript-eslint/no-non-null-assertion': 'off',
   }},
