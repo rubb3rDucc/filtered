5c7562359395ba706db68e5a59f7e3fabaa7d4fb
rubb3rDucc <thomalin@oregonstate.edu>
2024-11-04 21:16:11 -0500

    edit app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,27 +1,30 @@
-import express, { Express } from 'express';
+import dotenv from 'dotenv';
+import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
+dotenv.config();
+
 // import * as bodyParser from 'body-parser';
 // import * as cookieParser from 'cookie-parser';
-
+const port = process.env.PORT;
 const publicDir = process.argv[2] || __dirname + "/public";
-const allowedOrigins = ["http://localhost:*"];
+const allowedOrigins = `http://localhost:${port}`;
 
-const app: Express = express();
+const app = express();
 
 // Middleware
 app.use(cors());
-app.use(helmet());
-app.use(morgan(':id :method :url :response-time'));
 app.use(express.static(publicDir));
+app.use(helmet());
+app.use(morgan('dev'));
 // for cors
 app.use((req, res, next) => {
-    const origin = req.headers.origin!;
+    // const origin : string | undefined = req.headers.origin;
   
-    if (allowedOrigins.includes(origin)) {
-      res.header("Access-Control-Allow-Origin", origin);
+    if (allowedOrigins) {
+      res.header("Access-Control-Allow-Origin", allowedOrigins);
     }
   
     res.header("Access-Control-Allow-Origin", "*");
